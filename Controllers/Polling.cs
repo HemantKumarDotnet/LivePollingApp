7471c450960806573cc0ff580e3035147efe1f3b
HemantKumarDotnet <itshemantkumar9@gmail.com>
2024-11-11 00:34:10 +0530

    Startup
    11-11-2024

---
+++ Controllers/Polling.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LivePollingApp.Controllers
+{
+    public class Polling : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LivePollingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
